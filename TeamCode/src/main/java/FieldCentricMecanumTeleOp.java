55a842f19bf35a687d1835028196807ad4299a77
Neurobots <neurobots@etu.classomption.qc.ca>
2024-11-04 16:26:12 -0800

    PIDF

--- TeamCode/src/main/java/FieldCentricMecanumTeleOp.java
+++ TeamCode/src/main/java/FieldCentricMecanumTeleOp.java
@@ -64,63 +64,67 @@ public class FieldCentricMecanumTeleOp extends LinearOpMode {
         if (isStopRequested()) return;
 
         while (opModeIsActive()) {
-            double y = -gamepad1.left_stick_y; // Remember, Y stick value is reversed
-            double x = gamepad1.left_stick_x;
-            double rx = gamepad1.right_stick_x;
 
-            // This button choice was made so that it is hard to hit on accident,
-            // it can be freely changed based on preference.
-            // The equivalent button is start on Xbox-style controllers.
-            if (gamepad1.options) {
-                imu.resetYaw();
-            }
+            if (gamepad1.b) {
+                double y = -gamepad1.left_stick_y; // Remember, Y stick value is reversed
+                double x = gamepad1.left_stick_x;
+                double rx = gamepad1.right_stick_x;
 
-            double botHeading = imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.RADIANS);
+                // This button choice was made so that it is hard to hit on accident,
+                // it can be freely changed based on preference.
+                // The equivalent button is start on Xbox-style controllers.
+                if (gamepad1.options) {
+                    imu.resetYaw();
+                }
 
-            // Rotate the movement direction counter to the bot's rotation
-            double rotX = x * Math.cos(-botHeading) - y * Math.sin(-botHeading);
-            double rotY = x * Math.sin(-botHeading) + y * Math.cos(-botHeading);
+                double botHeading = imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.RADIANS);
 
-            rotX = rotX * 1.1;  // Counteract imperfect strafing
+                // Rotate the movement direction counter to the bot's rotation
+                double rotX = x * Math.cos(-botHeading) - y * Math.sin(-botHeading);
+                double rotY = x * Math.sin(-botHeading) + y * Math.cos(-botHeading);
 
-            // Denominator is the largest motor power (absolute value) or 1
-            // This ensures all the powers maintain the same ratio,
-            // but only if at least one is out of the range [-1, 1]
-            double denominator = Math.max(Math.abs(rotY) + Math.abs(rotX) + Math.abs(rx), 1);
-            double frontLeftPower = (rotY + rotX + rx) / denominator;
-            double backLeftPower = (rotY - rotX + rx) / denominator;
-            double frontRightPower = (rotY - rotX - rx) / denominator;
-            double backRightPower = (rotY + rotX - rx) / denominator;
+                rotX = rotX * 1.1;  // Counteract imperfect strafing
 
-            leftFront.setPower(frontLeftPower);
-            leftBack.setPower(backLeftPower);
-            rightFront.setPower(frontRightPower);
-            rightBack.setPower(backRightPower);
-            //ceci est un test;
+                // Denominator is the largest motor power (absolute value) or 1
+                // This ensures all the powers maintain the same ratio,
+                // but only if at least one is out of the range [-1, 1]
+                double denominator = Math.max(Math.abs(rotY) + Math.abs(rotX) + Math.abs(rx), 1);
+                double frontLeftPower = (rotY + rotX + rx) / denominator;
+                double backLeftPower = (rotY - rotX + rx) / denominator;
+                double frontRightPower = (rotY - rotX - rx) / denominator;
+                double backRightPower = (rotY + rotX - rx) / denominator;
+
+                leftFront.setPower(frontLeftPower);
+                leftBack.setPower(backLeftPower);
+                rightFront.setPower(frontRightPower);
+                rightBack.setPower(backRightPower);
+                //ceci est un test;
+                telemetry.update();
+            } else {
+
+                if (gamepad1.a) {
+                    controller = new PIDController(p, i, d);
+                    telemetry = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
+
+
+                    slidemotorright = hardwareMap.get(DcMotorEx.class, "slidemotorright");
+                    slidemotorleft = hardwareMap.get(DcMotorEx.class, "slidemotorleft");
+
+
+                    controller.setPID(p, i, d);
+                    int slidePos = slidemotorright.getCurrentPosition();
+                    double pid = controller.calculate(slidePos, target);
+                    double ff = Math.cos(Math.toRadians(target / ticks_in_degree)) * f;
+
+                    double power = pid + ff;
+
+                    slidemotorright.setPower(power);
+                    slidemotorleft.setPower(-power);
+                    telemetry.addData("pos", slidePos);
+                    telemetry.addData("target", target);
                     telemetry.update();
-
-                    if (gamepad1.a){
-                        controller = new PIDController(p, i, d);
-                        telemetry = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
-
-
-                        slidemotorright = hardwareMap.get(DcMotorEx.class, "slidemotorright");
-                        slidemotorleft = hardwareMap.get(DcMotorEx.class, "slidemotorleft");
-
-
-                        controller.setPID(p, i, d);
-                        int slidePos = slidemotorright.getCurrentPosition();
-                        double pid = controller.calculate(slidePos, target);
-                        double ff = Math.cos(Math.toRadians(target / ticks_in_degree)) * f;
-
-                        double power = pid + ff;
-
-                        slidemotorright.setPower(power);
-                        slidemotorleft.setPower(-power);
-                        telemetry.addData("pos", slidePos);
-                        telemetry.addData("target", target);
-                        telemetry.update();
-                    }
+                }
+            }
 
                 }
             }
