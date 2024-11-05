c6ffa9d0868c11dd390c91b6e8b0b304581821e3
Neurobots <neurobots@etu.classomption.qc.ca>
2024-11-04 16:04:56 -0800

    PIDF

--- TeamCode/src/main/java/FieldCentricMecanumTeleOp.java
+++ TeamCode/src/main/java/FieldCentricMecanumTeleOp.java
@@ -18,7 +18,7 @@ public class FieldCentricMecanumTeleOp extends LinearOpMode {
     public static double p = 0.005, i = 0, d = 0.0001;
     public static double f = -0.05;
 
-    public static int target = 300;
+    public static int target = -1500;
 
     private final double ticks_in_degree = 384.5 / 180.0;
 
