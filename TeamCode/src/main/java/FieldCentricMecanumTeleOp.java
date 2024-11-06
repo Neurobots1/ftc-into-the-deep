c20e8540851d8cae8f5fea8d12036a6e614f850e
Neurobots <neurobots@etu.classomption.qc.ca>
2024-11-06 14:52:50 -0800

    PIDF

--- TeamCode/src/main/java/FieldCentricMecanumTeleOp.java
+++ TeamCode/src/main/java/FieldCentricMecanumTeleOp.java
@@ -10,21 +10,23 @@ import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.IMU;
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.teamcode.ViperslidePIDF;
 
 @Config
 @TeleOp
 public class FieldCentricMecanumTeleOp extends LinearOpMode {
+    private Object ViperslidePIDF;
 
-    private PIDController controller;
-    public static double p = 0.005, i = 0, d = 0.0001;
-    public static double f = -0.05;
+    //private PIDController controller;
+    //public static double p = 0.005, i = 0, d = 0.0001;
+    //public static double f = -0.05;
 
-    public int target = -1200;
+    //public int target = -1200;
 
-    private final double ticks_in_degree = 384.5 / 180.0;
+    //private final double ticks_in_degree = 384.5 / 180.0;
 
-    private DcMotorEx slidemotorright;
-    private DcMotorEx slidemotorleft;
+    //private DcMotorEx slidemotorright;
+    //private DcMotorEx slidemotorleft;
 
     @Override
     public void runOpMode() throws InterruptedException {
@@ -101,26 +103,30 @@ public class FieldCentricMecanumTeleOp extends LinearOpMode {
             telemetry.update();
 
             if (gamepad1.y) {
-                controller = new PIDController(p, i, d);
-                telemetry = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
 
 
-                slidemotorright = hardwareMap.get(DcMotorEx.class, "slidemotorright");
-                slidemotorleft = hardwareMap.get(DcMotorEx.class, "slidemotorleft");
+                //controller = new PIDController(p, i, d);
+                //telemetry = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
 
 
-                controller.setPID(p, i, d);
-                int slidePos = slidemotorright.getCurrentPosition();
-                double pid = controller.calculate(slidePos, target);
-                double ff = Math.cos(Math.toRadians(target / ticks_in_degree)) * f;
+                //slidemotorright = hardwareMap.get(DcMotorEx.class, "slidemotorright");
+                //slidemotorleft = hardwareMap.get(DcMotorEx.class, "slidemotorleft");
 
-                double power = pid + ff;
+                //int target = -750;
 
-                slidemotorright.setPower(power);
-                slidemotorleft.setPower(-power);
-                telemetry.addData("pos", slidePos);
-                telemetry.addData("target", target);
-                telemetry.update();
+
+                //controller.setPID(p, i, d);
+                //int slidePos = slidemotorright.getCurrentPosition();
+                //double pid = controller.calculate(slidePos, target);
+                //double ff = Math.cos(Math.toRadians(target / ticks_in_degree)) * f;
+
+                //double power = pid + ff;
+
+                //slidemotorright.setPower(power);
+                //slidemotorleft.setPower(-power);
+                //telemetry.addData("pos", slidePos);
+                //telemetry.addData("target", target);
+                //telemetry.update();
 
 
             }
