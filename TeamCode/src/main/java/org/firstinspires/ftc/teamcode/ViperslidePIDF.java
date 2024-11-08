f6d65e574dc91cf4045ce73dba7fc4d0e41dfa15
Neurobots <neurobots@etu.classomption.qc.ca>
2024-11-08 15:20:44 -0800

    PIDF

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ViperslidePIDF.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ViperslidePIDF.java
@@ -13,7 +13,7 @@ public class ViperslidePIDF extends OpMode {
     private PIDController controller;
 
 
-    public static double p = 0.005, i = 0, d = 0.0001;
+    public static double p = 0.001, i = 0, d = 0.0001;
     public static double f = -0.05;
 
     public static int target;
@@ -36,18 +36,18 @@ public class ViperslidePIDF extends OpMode {
     public void loop() {
         if (gamepad1.a) {
             target = -500;
-            controller.setPID(p, i, d);
-            int slidePos = slidemotorright.getCurrentPosition();
-            double pid = controller.calculate(slidePos, target);
-            double ff = Math.cos(Math.toRadians(target / ticks_in_degree)) * f;
-
-            double power = pid + ff;
-
-            slidemotorright.setPower(power);
-            slidemotorleft.setPower(-power);
-            telemetry.addData("pos", slidePos);
-            telemetry.addData("target", target);
-            telemetry.update();
         }
+        controller.setPID(p, i, d);
+        int slidePos = slidemotorright.getCurrentPosition();
+        double pid = controller.calculate(slidePos, target);
+        double ff = Math.cos(Math.toRadians(target / ticks_in_degree)) * f;
+
+        double power = pid + ff;
+
+        slidemotorright.setPower(power);
+        slidemotorleft.setPower(-power);
+        telemetry.addData("pos", slidePos);
+        telemetry.addData("target", target);
+        telemetry.update();
     }
 }
