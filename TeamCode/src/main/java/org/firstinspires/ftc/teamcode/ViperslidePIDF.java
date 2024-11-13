000a08b2af701951351ce672a4c3cb4f2c98cbf9
Neurobots <neurobots@etu.classomption.qc.ca>
2024-11-13 14:35:45 -0800

    PIDF

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ViperslidePIDF.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ViperslidePIDF.java
@@ -13,10 +13,10 @@ public class ViperslidePIDF extends OpMode {
     private PIDController controller;
 
 
-    public static double p = 0.001, i = 0, d = 0.0001;
-    public static double f = -0.05;
+    public static double p = 0.005, i = 0, d = 0.0001;
+    public static double f = -0.1;
 
-    public static int target;
+    public static int target = 0;
 
     private final double ticks_in_degree = 384.5 / 180.0;
 
@@ -34,20 +34,45 @@ public class ViperslidePIDF extends OpMode {
 
     @Override
     public void loop() {
-        if (gamepad1.a) {
-            target = -500;
+        if (gamepad1.y) {
+            target = -2000;
+            controller.setPID(p, i, d);
+            int slidePos = slidemotorright.getCurrentPosition();
+            double pid = controller.calculate(slidePos, target);
+            double ff = Math.cos(Math.toRadians(target / ticks_in_degree)) * f;
+
+            double power = pid + ff;
+
+            slidemotorright.setPower(power);
+            slidemotorleft.setPower(-power);
+            telemetry.addData("pos", slidePos);
+            telemetry.addData("target", target);
+            telemetry.update();
+        } else {
+            controller.setPID(p, i, d);
+            int slidePos = slidemotorright.getCurrentPosition();
+            double pid = controller.calculate(slidePos, target);
+            double ff = Math.cos(Math.toRadians(target / ticks_in_degree)) * f;
+
+            double power = pid + ff;
+
+            slidemotorright.setPower(power);
+            slidemotorleft.setPower(-power);
+            telemetry.addData("pos", slidePos);
+            telemetry.addData("target", target);
+            telemetry.update();
+
+            if (gamepad1.a){
+                target = -50;
+            }
+
+            if (gamepad1.x){
+                target = -900;
+            }
+
+            if (gamepad1.b){
+                target = -1400;
+            }
         }
-        controller.setPID(p, i, d);
-        int slidePos = slidemotorright.getCurrentPosition();
-        double pid = controller.calculate(slidePos, target);
-        double ff = Math.cos(Math.toRadians(target / ticks_in_degree)) * f;
-
-        double power = pid + ff;
-
-        slidemotorright.setPower(power);
-        slidemotorleft.setPower(-power);
-        telemetry.addData("pos", slidePos);
-        telemetry.addData("target", target);
-        telemetry.update();
     }
 }
